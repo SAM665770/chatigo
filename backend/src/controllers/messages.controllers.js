247ecd7f2a2cef1faf1aa28d03ed0a376da15308
Samridh Palleda <psamridh007@gmail.com>
2025-11-17 23:03:19 +0530

    message routes optimized

--- backend/src/controllers/messages.controllers.js
+++ backend/src/controllers/messages.controllers.js
@@ -41,6 +41,16 @@ export const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    if (!text || !image)
+      return res.status(400).json({ message: "Please provide text or image" });
+    if (senderId.equals(receiverId))
+      return res
+        .status(400)
+        .json({ message: "You can't send message to yourself" });
+    const receiverExists = await User.findById(receiverId);
+    if (!receiverExists)
+      return res.status(404).json({ message: "Receiver not found" });
+
     let imageUrl;
     if (image) {
       // upload base64 image to cloudinary
